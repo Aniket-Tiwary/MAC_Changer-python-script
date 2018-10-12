2f00ade1e272348fa5169ceb97abe4edf4505eda
Aniket-Tiwary <32029080+Aniket-Tiwary@users.noreply.github.com>
2018-10-12 18:53:01 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,14 @@
 #!usr/share/bin/env python3
 
+
+#Three libraries are used implement the script
 import subprocess
 import optparse
 import re
 
+
+
+#This function takes arguments (as evident from the function's name)and returns them. 
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest = "interface", help = "Enter the interface to change mac address")
@@ -15,11 +20,18 @@ def get_arguments():
         parser.error("[+]Specify the MAC Address")
     return options
 
+
+#This block uses the subprocess library to cahnge the MAC address according to the user. 
+#subprocess library is used to implement terminal command using python
+
 def change_mac(interface,new_mac):
     subprocess.call(["ifconfig",interface,"down"])
     subprocess.call(["ifconfig",interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig",interface,"up"])
     subprocess.call(["ifconfig",interface])
+    
+
+#This takes the mac address provided by user and checks if its valid and also the desired output was achieved or not     
 
 def get_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
@@ -28,12 +40,12 @@ def get_mac(interface):
         print(mac_address_search_result.group(0))
     else:
         print("[-] Could not read mac address")
+        
+        
 
 options = get_arguments()
-
 current_mac = get_mac(options.interface)
 print("Current Mac = " + str(current_mac))
-
 change_mac(options.interface,options.new_mac)
 new_mac = get_mac(options.interface)
 if current_mac == new_mac:
